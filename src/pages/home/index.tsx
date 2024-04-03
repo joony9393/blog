25e5e65e7fdda01946f64afa143a8d73f4f88f00
[SW8] 정영준 <wjddudwns567@gmail.com>
2024-04-03 11:09:22 +0900

    first init

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <div>
+      <header>
+        <div>
+          <Link to="/posts/new">글쓰기</Link>
+          <Link to="/posts">게시글</Link>
+          <Link to="/profile">프로필</Link>
+        </div>
+      </header>
+      <div className="post__list">
+        {[...Array(10)].map((e, index) => (
+          <div key={index} className="post__box">
+            <Link to={`/posts/${index}`}>
+              <div className="post__profile-box">
+                <div className="post__profile" />
+                <div className="post__author-name">패스트캠퍼스</div>
+                <div className="post__date"></div>
+              </div>
+              <div className="post__title">게시글</div>
+            </Link>
+          </div>
+        ))}
+      </div>
+      <footer>
+        <div>Menu 1</div>
+        <div>Menu 2</div>
+        <div>Menu 3</div>
+      </footer>
+    </div>
+  );
+}
